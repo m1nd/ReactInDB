d42eaabff21f66e607fc74143e8f0412fdbda692
m1nd <m1nd@mail.ru>
2017-09-11 11:35:17 +0300

    Update TabContainer

--- src/components/TabsContainer.js
+++ src/components/TabsContainer.js
@@ -12,23 +12,57 @@ export default class TabsContainer extends Component {
     };
 
     this.props.store.getAllData().then(item => this.setState({ data: item }));
+    this.convert = this.convert.bind(this);
+    // console.log(this.props.store.data);
+    this.store = {};
+
+    // const obj = {};
+    // store.map((item, index, arr) => {
+    //   if (index === 0) obj[item.tabId] = [{ name: item.name, checked: item.checked }];
+    //   else if (arr[index-1].tabId === item.tabId) obj[item.tabId].push({ name: item.name, checked: item.checked });
+    //   else obj[item.tabId] = [{ name: item.name, checked: item.checked }];
+    // });
+
+  }
+
+  convert(data) {
+    const obj = {};
+    data.map((item, index, arr) => {
+      if (index === 0) obj[item.tabId] = [{ name: item.name, checked: item.checked }];
+      else if (arr[index-1].tabId === item.tabId) obj[item.tabId].push({ name: item.name, checked: item.checked });
+      else obj[item.tabId] = [{ name: item.name, checked: item.checked }];
+    });
+    return (
+      <Tabs s={8}>
+        { Object.keys(obj).map( item => {
+            return ( 
+              <Tab title={item} > 
+                { obj[item].map( el => {
+                    return (
+                      <TabItem label={el.name} checked={el.checked}/>
+                    )
+                  }) 
+                }
+              </Tab>
+              )   
+          }) 
+        }
+      </Tabs>
+    );
+  }
+
+  componentDidUpdate() {
+    // console.log(this.state.data); 
+    // this.setState({ name: 123 });
   }
 
   render() {
     return (
       <Col s={12} className="center-align">
-        <Tabs s={8}>
-          {this.state.data.map(
-            (item, index, arr) =>
-              index > 0 && arr[index - 1].tabId === item.tabId ? (
-                <TabItem value={item.name} check={item.checked} />
-              ) : (
-                <Tab title={item.tabId} key={index}>
-                  <TabItem value={item.name} check={item.checked} />
-                </Tab>
-              ),
-          )}
-        </Tabs>
+      { this.state.data.length === 0 ? '' : 
+          this.convert(this.state.data)            
+
+      }
       </Col>
     );
   }
